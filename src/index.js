2148271245d6fa06f4467e0b74ed70226bd4180d
juliettemangon2 <160437127+juliettemangon2@users.noreply.github.com>
2025-04-24 19:27:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';   // ← use HashRouter
+import App from './App';
 
-const basename = process.env.PUBLIC_URL || '/'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={basename}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
-)
+  </HashRouter>
+);
