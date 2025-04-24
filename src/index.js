330f2235fb81c7a1badd0eb9871e00c82c9a0e68
juliettemangon2 <160437127+juliettemangon2@users.noreply.github.com>
2025-04-24 19:25:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+const basename = process.env.PUBLIC_URL || '/'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 )
