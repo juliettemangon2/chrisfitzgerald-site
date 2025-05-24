841978197de581fa542d89773ccdc752f083eb47
juliettemangon2 <160437127+juliettemangon2@users.noreply.github.com>
2025-05-23 21:48:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from './LandingPage'
-import AboutPage from './AboutPage'
-import PhotographyPage from './PhotographyPage'
-import ContactPage from './ContactPage'
-import ShootPage from './ShootPage'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import AboutPage from './AboutPage';
+import PhotographyPage from './PhotographyPage';
+import ShootPage from './ShootPage';
+import ContactPage from './ContactPage';
 
-
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/photography" element={<PhotographyPage />} />
-      <Route path="/contact" element={<ContactPage />} />
       <Route path="/photography/:projectId" element={<ShootPage />} />
-
+      <Route path="/contact" element={<ContactPage />} />
     </Routes>
-  )
+  );
 }
-
-export default App
